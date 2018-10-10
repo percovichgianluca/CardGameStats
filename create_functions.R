57109ca475ab0ffed4678d2d64cf8b239f8bdc0e
stevebroll <stevebroll@github.com>
2018-10-10 13:31:35 -0500

    replace 'Face' with '10' in deck, add blackjack() function

--- create_functions.R
+++ create_functions.R
@@ -1,11 +1,12 @@
 # Restore deck function --------------------------------------------------------
 restore_deck <- function(){
   deck_var <- c(rep.int('Ace', 4), 
-                rep.int(as.character(2:10), 4), 
-                rep.int('Face', 12))
+                rep.int(as.character(2:9), 4), 
+                rep.int('10', 16))
+  return(deck_var)
 }
 
-# Draw and remove cards function --------------------------------------------------------
+# Draw and remove cards function -----------------------------------------------
 draw_cards <- function(hand_var, deck_var, draw_size){
   for (i in 1:draw_size){
     card <- sample(deck_var, size = 1)
@@ -15,6 +16,32 @@ draw_cards <- function(hand_var, deck_var, draw_size){
   return(list(hand_var, deck_var))
 }
 
+# Blackjack --------------------------------------------------------------------
+blackjack <- function(player_hand, dealer_hand){
+  # Check for blackjacks =======================================================
+  b1 <- c('10', 'Ace')
+  b2 <- c('Ace', '10')
+  blackjack_player = blackjack_dealer <- FALSE
+  if (identical(player_hand, b1) | identical(player_hand, b2)){
+    blackjack_player = TRUE
+  }
+  if (identical(dealer_hand, b1) | identical(dealer_hand, b2)){
+    blackjack_dealer = TRUE
+  }
+  # Output for victor ==========================================================
+  if (blackjack_player & blackjack_dealer){
+    cat('DRAW!\n')
+  }
+  else if (blackjack_player & !blackjack_dealer){
+    cat('PLAYER WINS!\n')
+  }
+  else if (!blackjack_player & blackjack_dealer){
+    cat('DEALER WINS!\n')
+  }
+  
+  continue_check <- !(blackjack_player | blackjack_dealer) # False if blackjack
+  return(continue_check)
+}
 
 
 
